d4d92da89f28df85a93f9155e25caf59ef16ee32
Holidoli <liandongwei@gmail.com>
2020-05-11 12:15:53 -0400

    Add the Moon cycle

---
+++ Assets/William/Script/DayNight/MoonModule.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonModule : DNMouduleBase
+{
+    [SerializeField] private Light Moon;
+    [SerializeField] Gradient MoonColor;
+    [SerializeField] private float BaseIntensity;
+
+    public override void UpdateModule(float intensity)
+    {
+        Moon.color = MoonColor.Evaluate(1 - intensity);
+        Moon.intensity = (1 - intensity * BaseIntensity + 0.05f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/William/Script/DayNight/SkyBoxModule.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBoxModule : DNMouduleBase
+{
+    [SerializeField] private Gradient SkyColor;
+    [SerializeField] private Gradient HorizonColor;
+
+    public override void UpdateModule(float intensity)
+    {
+        RenderSettings.skybox.SetColor("_SkyTint", SkyColor.Evaluate(intensity));
+        RenderSettings.skybox.SetColor("_GrandColor", HorizonColor.Evaluate(intensity));
+    }
+    
+}
