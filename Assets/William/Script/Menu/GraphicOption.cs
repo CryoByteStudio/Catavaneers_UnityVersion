fb58040ccf92a2890ed138eaa24a6fdc83aeff6c
Holidoli <liandongwei@gmail.com>
2020-05-18 19:24:35 -0400

    revisit the option menu

---
+++ Assets/William/Script/Menu/GraphicOption.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GraphicOption : MonoBehaviour
+{
+    public Toggle fullScreenTog, vsyncTog;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if(Screen.fullScreen == true)
+        //{
+        //    fullScreenTog.isOn = true;
+        //}
+        //else
+        //{
+        //    fullScreenTog.isOn = false;
+        //}
+
+        //if(QualitySettings.vSyncCount == 1)
+        //{
+        //    vsyncTog.isOn = true;
+        //}
+        //else
+        //{
+        //    vsyncTog.isOn = false;
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ApplyGraphics()
+    {
+        Screen.fullScreen = fullScreenTog.isOn;
+
+        if(vsyncTog.isOn)
+        {
+            QualitySettings.vSyncCount = 1;
+        }
+        else
+        {
+            QualitySettings.vSyncCount = 0;
+        }
+    }
+}
