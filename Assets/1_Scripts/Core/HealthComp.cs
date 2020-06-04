29d255d353ecd9e595dcb94d2c3ac433565c4a33
Mandalore477 <sithlordjazz@gmail.com>
2020-06-04 16:58:22 -0400

    fixed russel debug  problem

--- Assets/1_Scripts/Core/HealthComp.cs
+++ Assets/1_Scripts/Core/HealthComp.cs
@@ -10,7 +10,7 @@ using ViTiet.Utils;
 using Catavaneer;
 
 public enum CharacterClass { Player, Enemy, Caravan, Obj };
-public enum DifficultyLevel { Normal, IronCat, Catapocalypse, Catfight};
+public enum DifficultyLevel { Normal = 5, IronCat = 10, Catapocalypse = 25, Catfight = 1};
 
 
 
@@ -173,7 +173,14 @@ public class HealthComp : MonoBehaviour
     {
         if (debug)
             TestTakeDamage();
-
+        if(SpawnManager.EnemiesAlive == 0)
+        {
+            SetIsWaveComplete(true);
+        }
+        else
+        {
+            SetIsWaveComplete(false);
+        }
         timeElapsed += Time.deltaTime;
         if (myClass == CharacterClass.Caravan && is_Regenerating) {
             //dmg_percentage = currentHealth % (startHealth / (int)gameDifficulty);
