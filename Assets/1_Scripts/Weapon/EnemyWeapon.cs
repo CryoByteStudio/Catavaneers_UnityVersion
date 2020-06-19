a2d39ecb7fed7b257c811964e1d22ab5898f282b
avaziri87 <alejandrovaziri@gmail.com>
2020-06-19 13:49:33 -0400

    enemy weapons
    all weapon model adjusted the transform to the enemy models, cats have a specific setting diferent from dog and mouse.
    
    enemy scriptable object adjusted for the enemies to use enemy overrider and enemy prefab, values so far are the same left to be adjust by game desing

--- Assets/1_Scripts/Weapon/EnemyWeapon.cs
+++ Assets/1_Scripts/Weapon/EnemyWeapon.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class EnemyWeapon : MonoBehaviour
 {
-    [SerializeField] Weapon defaultWeapon = null;
+    [SerializeField] Weapon[] weapons = null;
+    [SerializeField] float[] spawnChance = null;
     [SerializeField] Weapon currentWeapon = null;
     [SerializeField] Transform rightHandTransform = null;
     [SerializeField] Transform leftHandTransform = null;
     void Start()
     {
+        float randomWeapon = Random.Range(0,100);
         if (currentWeapon == null)
         {
-            EquipWeapon(defaultWeapon);
+            if (randomWeapon >= spawnChance[2])         currentWeapon = weapons[3];
+            else if (randomWeapon >= spawnChance[1])    currentWeapon = weapons[2];
+            else if (randomWeapon >= spawnChance[0])    currentWeapon = weapons[1];
+            else                                        currentWeapon = weapons[0];
         }
+        EquipWeapon(currentWeapon);
     }
     public void EquipWeapon(Weapon weapon)
     {
         currentWeapon = weapon;
-        GetComponent<PlayerController>().SetWeaponWeight(currentWeapon.GetWeaponWeight());
         Animator animator = GetComponent<Animator>();
         weapon.Spawn(rightHandTransform, leftHandTransform, animator);
     }
