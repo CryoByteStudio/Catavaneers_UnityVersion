e078a8a5e9d4cba4113d3864fab4d582c39b2b5a
avaziri87 <alejandrovaziri@gmail.com>
2020-06-19 12:41:59 -0400

    first step for enemy weapon

---
+++ Assets/1_Scripts/Weapon/EnemyWeapon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWeapon : MonoBehaviour
+{
+    [SerializeField] Weapon defaultWeapon = null;
+    [SerializeField] Weapon currentWeapon = null;
+    [SerializeField] Transform rightHandTransform = null;
+    [SerializeField] Transform leftHandTransform = null;
+    void Start()
+    {
+        if (currentWeapon == null)
+        {
+            EquipWeapon(defaultWeapon);
+        }
+    }
+    public void EquipWeapon(Weapon weapon)
+    {
+        currentWeapon = weapon;
+        GetComponent<PlayerController>().SetWeaponWeight(currentWeapon.GetWeaponWeight());
+        Animator animator = GetComponent<Animator>();
+        weapon.Spawn(rightHandTransform, leftHandTransform, animator);
+    }
+}
