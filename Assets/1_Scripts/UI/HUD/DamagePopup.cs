ac38935495b6b5e831c4d8271554648ac299113a
Vi Tiet <vitiet.programmer@gmail.com>
2020-06-21 21:12:17 -0400

    Damage Popup Update

--- Assets/1_Scripts/UI/HUD/DamagePopup.cs
+++ Assets/1_Scripts/UI/HUD/DamagePopup.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using TMPro;
+using UnityEngine.UI;
+using Catavaneer.Extensions;
 
 public class DamagePopup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float animateDuration;
+    [SerializeField] private float fadeDuration;
+    [SerializeField] private float popHeight;
+    [SerializeField] Ease easeType;
+    [SerializeField] private MaskableGraphic graphicToFade;
+
+    private void Start()
     {
-        
+        Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Play()
     {
-        
+        transform.DOMoveY(popHeight, animateDuration).SetRelative().SetEase(easeType).
+            OnComplete (() =>
+            {
+                transform.DOMoveY(-popHeight, Mathf.Abs(animateDuration - fadeDuration)).SetRelative().SetEase(easeType);
+                graphicToFade.DOFade(0, fadeDuration);
+                Destroy(gameObject, Mathf.Max(animateDuration, fadeDuration));
+            });
+        transform.DOScale(3, animateDuration).SetEase(easeType);
     }
 }
