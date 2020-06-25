5b0e90b8baed00f2b543c4f92c250a5b0639c0aa
Vi Tiet <vitiet.programmer@gmail.com>
2020-06-25 16:03:49 -0400

    Add Button Smash Preventor

---
+++ Assets/1_Scripts/Player Interaction/ButtonSmashPreventor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ButtonSmashPreventor
+{
+    private static int limit = 1;
+    public static int Limit => limit;
+    public static void SetLimit(int value)
+    {
+        limit = value;
+    }
+
+    public static bool ShouldProceed(ref int currentPressCount)
+    {
+        currentPressCount++;
+        return currentPressCount - 1 < limit;
+    }
+}
