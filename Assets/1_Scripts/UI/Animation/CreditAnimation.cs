2a37fe0198cf0dd7f4b80a2880ebd8359f03ec42
Vi Tiet <vitiet.programmer@gmail.com>
2020-06-29 21:19:00 -0400

    Update Credit Text Animation & Update Shop Text Color & Update Spawnpoints

---
+++ Assets/1_Scripts/UI/Animation/CreditAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using Catavaneer.Extensions;
+
+public class CreditAnimation : MonoBehaviour
+{
+    [SerializeField] private float moveDistance;
+    [SerializeField] private float duration;
+    [SerializeField] private float delay;
+    [SerializeField] private List<MaskableGraphic> graphics;
+    [SerializeField] private MaskableGraphic endingText;
+
+    private void OnEnable()
+    {
+        foreach (var graphic in graphics)
+        {
+            graphic.DOFade(1, delay).From(0);
+        }
+
+        transform.DOMoveY(moveDistance, duration).SetEase(Ease.Linear).SetDelay(delay);
+        endingText.DOFade(1, 1).SetDelay(duration);
+    }
+}
--- Assets/1_Scripts/UI/Menu/New Menu System/CreditsMenu.cs
+++ Assets/1_Scripts/UI/Menu/New Menu System/CreditsMenu.cs
@@ -1,26 +1,54 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using ViTiet.Utils;
+using Catavaneer.Extensions;
+using DG.Tweening;
 
 namespace Catavaneer.MenuSystem
 {
     public class CreditsMenu : Menu<CreditsMenu>
     {
-        [SerializeField]
-        private GameObject firstSelected;
+        [SerializeField] private GameObject backButton;
+        [SerializeField] private float backDelay;
+        [SerializeField] private List<MaskableGraphic> graphicsToFade;
+        private float backTimer;
 
         #region UNITY ENGINE FUNCTIONS
-        protected override void Awake()
+        private void OnEnable()
         {
-            base.Awake();
-            SetSelectedGameObject(firstSelected);
+            BackButtonFadeOn();
         }
 
-        private void Update()
+        //private void OnEnable()
+        //{
+        //    backTimer = Time.time + backDelay;
+        //}
+
+        //private void Update()
+        //{
+        //    if (Input.anyKeyDown && Time.time >= backTimer)
+        //    {
+        //        OnBackPressed();
+        //    }
+        //}
+        #endregion
+
+        #region PRIVATE METHODS
+        private void BackButtonFadeOn()
         {
-            if (Input.anyKeyDown)
+            if (backButton)
+                graphicsToFade = backButton.transform.GetAllComponentsOfTypeInHierachy<MaskableGraphic>();
+
+            if (graphicsToFade != null && graphicsToFade.Count > 0)
             {
-                OnBackPressed();
+                float delay = backDelay / 3f;
+                foreach (var graphic in graphicsToFade)
+                {
+                    graphic.DOFade(0.75f, backDelay - delay).From(0).SetDelay(delay)
+                        .OnComplete(() => EventSystem.current.SetSelectedGameObject(backButton));
+                }
             }
         }
         #endregion
